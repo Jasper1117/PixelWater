4c09fadb62714a7c5cdaabd569c99a83175d8011
Jasper <jasperxu1117@gmail.com>
2022-05-09 18:08:20 +0800

    resize

--- src/Tree/Tree.js
+++ src/Tree/Tree.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Parallax } from 'react-scroll-parallax';
-import bowel from "../Faucet/img/NewNewBowel-01.svg"
+import bowel from "../Faucet/img/FinalFinalBowel-01.svg"
 import "../Tree/tree.css"
 
 const Tree = () => {
   return (
     <div className="tree3Container">
-    <Parallax >
+
+    <Parallax>
+
           <img src={bowel} alt="" className='bowel'/>
+         
       </Parallax>
-  
+
     </div>
   )
 }
